5571dceb286fbd41e26afacd60f5f766ff4d2875
sadderpro <deluke470@gmail.com>
2025-06-15 14:40:03 -0500

    Draws a rectangle that covers the whole canvas.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+// TODO: load a texture on the canvas
 
 
 // retrieving canvas, setting context.
@@ -19,51 +19,43 @@ async function main() {
     const fShader = await compiledShader(gl, gl.FRAGMENT_SHADER, fSource);
     const program = await createProgram(gl, vShader, fShader);
 
-
+    // creating and binding position buffer. Locating attribute a_position.
     const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // base for vertex array
+    // base for vertex array to draw triangle
     const positions = [
-        0,0,
-        0, 0.5,
-        0.7, 0
+        -1.0, 1.0,
+        -1.0, -1.0,
+        1.0, 1.0,
+        1.0, -1.0
     ]
 
+    // raw data loaded in the buffer
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
     const vertexArrayObject = gl.createVertexArray();
-
     gl.bindVertexArray(vertexArrayObject);
     gl.enableVertexAttribArray(positionAttributeLocation);
 
-    // ** data for vertex array pointer
-    // ** will likely go unchanged for this example, thus the const declaration for primitives
     const size = 2;
     const type = gl.FLOAT;
     const normalize = false;
     const stride = 0;
     const offset = 0;
-
-    // creating the vertex array pointer
     gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
-    // convert clip size values to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    // set whole canvas to black
+    // clear
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // now we'll actually use our program
+    // draw
     gl.useProgram(program);
     gl.bindVertexArray(vertexArrayObject);
 
-    const primitiveType = gl.TRIANGLES;
-    const count = 3;
-
-    gl.drawArrays(primitiveType, offset, count);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
 }
 
